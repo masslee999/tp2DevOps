9de1006a157fe344c4af9e114f68fd8b67e1cf08
kamil-dw <diawpmkamil@gmail.com>
2024-06-13 10:26:29 +0000

    enrégistrement 3 terminé

--- tache5.py
+++ tache5.py
@@ -7,3 +7,12 @@ else :
 
 a = "bonjour"
 print(a)
+
+def Sum_moy(LISTE):
+    Sum = 0
+    for val in LISTE:
+        Sum += val
+    moy = round(Sum/len(LISTE), 1)
+    return Sum, moy 
+
+Sum_moy([-1, 3, 4, -1])   
