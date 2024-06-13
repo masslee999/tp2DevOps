7f9d36bc680c5ddc1722bbbea10deaf95df474b9
MaimounaS-git <maimounasow1410@gmail.com>
2024-06-13 09:41:59 +0000

    may: fini

--- Tache7.py
+++ Tache7.py
@@ -1,5 +1,6 @@
-def viceversa(D1):
-    dictionnaire = {}
-    for cle, valeur in D1.items():
-        dictionnaire[valeur] = cle
-    return dictionnaire
+def filtre(dico):
+  dico_filtre = {}  # Dictionnaire vide pour stocker les éléments filtrés
+  for key, valeur in dico.items():
+    if len(valeur) > 5:  # Vérification de la longueur de la chaîne
+      dico_filtre[key] = valeur  # Ajout au dictionnaire filtré
+  return dico_filtre
--- tache8.py
+++
@@ -1,5 +0,0 @@
-def viceversa(D1):
-    dictionnaire = {}
-    for cle, valeur in D1.items():
-        dictionnaire[valeur] = cle
-    return dictionnaire
